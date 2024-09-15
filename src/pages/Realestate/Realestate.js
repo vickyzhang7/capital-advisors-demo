4f4de822f1240c43d00af9a808b1043a0c5696d4
weiqi <weiqi997998@gmail.com>
2024-09-15 17:44:11 -0400

    Key

--- src/pages/Realestate/Realestate.js
+++ src/pages/Realestate/Realestate.js
@@ -16,8 +16,8 @@ const Realestate = () => {
   
   
   AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
   region: 'us-east-2',
   });
 
