affdcb4c1d74227902268af4e3d7970a61e21f0c
weiqi <weiqi997998@gmail.com>
2024-09-16 16:38:39 -0400

    stockprice

--- install_dependencies.py
+++ install_dependencies.py
@@ -2,7 +2,7 @@ import json
 import subprocess
 
 # Load dependencies from JSON
-with open('dependencies.json', 'r') as file:
+with open('package.json', 'r') as file:
     data = json.load(file)
 
 # Install dependencies
