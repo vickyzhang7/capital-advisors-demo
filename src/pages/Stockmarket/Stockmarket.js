ba28391c5bc2ddd49f8706b25f5e7719cb9389b3
weiqi <weiqi997998@gmail.com>
2024-09-16 17:35:06 -0400

    Connect

--- src/pages/Stockmarket/Stockmarket.js
+++ src/pages/Stockmarket/Stockmarket.js
@@ -7,7 +7,7 @@ const Stockmarket = () => {
 
   const fetchStockData = async (symbol) => {
     try {
-      const response = await fetch(`http://127.0.0.1:5000/stock/${symbol}`);
+      const response = await fetch(`https://capital-advisors-demo.onrender.com/stock/${symbol}`);
       const data = await response.json();
       setStockData(data);
     } catch (error) {
@@ -16,7 +16,7 @@ const Stockmarket = () => {
   };
   
   // http://127.0.0.1:5000/stock/${symbol}`
-  //https://capital-advisors-demo-1.onrender.com/stock/${symbol}
+  //https://capital-advisors-demo.onrender.com/stock/${symbol}
 
   
   useEffect(() => {
